0a5012b3908766805a8e8e01ceec842a35d3a0e9
Avnesh Shakya <avnesh.nitk@gmail.com>
2016-04-22 23:07:04 +0530

    login api integration

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, loginService, $state) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -8,6 +8,12 @@ angular.module('starter.controllers', [])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  $scope.init = function () {
+    loginService.getData().then(function(response) {
+        $scope.loginDetails = response.data.login;
+        console.log($scope.loginDetails);
+    });
+  };
 
   // Form data for the login modal
   $scope.loginData = {};
@@ -32,6 +38,15 @@ angular.module('starter.controllers', [])
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
+    for(var i=0; i<$scope.loginDetails.length; i++) {
+      console.log($scope.loginDetails[i].email, $scope.loginDetails[i].password);
+      if($scope.loginDetails[i].email === $scope.loginData.username && $scope.loginDetails[i].password === $scope.loginData.password) {
+        console.log("login successfully");
+        $state.go('app.search');
+      }else {
+        console.log("Invalid Username or Password");
+      }
+    }
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
@@ -39,9 +54,11 @@ angular.module('starter.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
+
+  $scope.init();
 })
 
-.controller('PlaylistsCtrl', function($scope, clientId) {
+.controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
     { title: 'Chill', id: 2 },
@@ -50,7 +67,7 @@ angular.module('starter.controllers', [])
     { title: 'Rap', id: 5 },
     { title: 'Cowbell', id: 6 }
   ];
-  console.log(clientId);
+  // console.log(clientId);
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,19 @@
 angular.module('starter.services', [])
-.factory('clientId', function clientIdFactory() {
-  return 'a12345654321x';
+
+/**
+ * @ngdoc service
+ * @name loginService
+ * @description Getting user data for login cred
+ */
+
+.factory('loginService', function($http) {
+    return{
+    	getData: function() {
+	        return $http.get('./data/loginData.json').then(function(response) {
+	            return response;
+	        }).catch(function(response){
+	         return
+	      })
+	    }
+    };
 });
