dad705597b4c663bd00e0ecda96f3134b2c6b72c
kaushik <kaushikgotefode@gmail.com>
2016-04-23 16:24:09 +0530

    login page

--- www/js/controllers/LoginCtrl.js
+++ www/js/controllers/LoginCtrl.js
@@ -1,35 +1,46 @@
-(function(){
-	'use strict';
+(function() {
+    'use strict';
 
-	angular.module('starter.controllers').controller('LoginCtrl', ['$scope', '$timeout', '$state', function($scope, $timeout, $state){
-		
-		$scope.loginData = {};
-		// Create the login modal that we will use later
-		// $ionicModal.fromTemplateUrl('templates/login.html', {
-		// 	scope: $scope
-		// }).then(function(modal) {
-		// 	$scope.modal = modal;
-		// });
+    angular.module('starter.controllers').controller('LoginCtrl', ['$scope', '$timeout', '$state','loginService', function($scope, $timeout, $state,loginService) {
 
-		// Triggered in the login modal to close it
-		// $scope.closeLogin = function() {
-		// 	$scope.modal.hide();
-		// };
+        $scope.loginData = {};
 
-		// Open the login modal
-		// $scope.login = function() {
-		// 	$scope.modal.show();
-		// };
+        loginService.getData().then(function(response) {
+            $scope.loginData = response;
+        });
+        // Create the login modal that we will use later
+        // $ionicModal.fromTemplateUrl('templates/login.html', {
+        // 	scope: $scope
+        // }).then(function(modal) {
+        // 	$scope.modal = modal;
+        // });
 
-		// Perform the login action when the user submits the login form
-		$scope.doLogin = function() {
-			console.log('Doing login', $scope.loginData);
-			// Simulate a login delay. Remove this and replace with your login
-			// code if using a login system
-			$timeout(function() {
-				$state.go('app.adopt', {});
-			}, 1000);
-		};
+        // Triggered in the login modal to close it
+        // $scope.closeLogin = function() {
+        // 	$scope.modal.hide();
+        // };
 
-	}]);
+        // Open the login modal
+        // $scope.login = function() {
+        // 	$scope.modal.show();
+        // };
+
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function() {
+            console.log('Doing login', $scope.loginData);
+            var loginUser = $scope.loginData.data.login;
+	        for (var i = 0; i < loginUser.length; i++) {
+	            var user = loginUser[i];
+	            if (user.email == $scope.usrname && user.password == $scope.psswrd) {
+	                // SharedDataService.setCurrentUser(user);
+	                $state.go('app.adopt');
+	                break;
+	            } else {
+	                $scope.errorMsg = true;
+	            }
+	        }
+            
+        };
+
+    }]);
 })();
