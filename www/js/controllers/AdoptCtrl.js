97c2252823f7e77438e268fe8e877b776d3d058a
kaushik <kaushikgotefode@gmail.com>
2016-04-24 02:40:34 +0530

    dashboard and treeprofile data binding

--- www/js/controllers/AdoptCtrl.js
+++ www/js/controllers/AdoptCtrl.js
@@ -2,7 +2,7 @@
     'use strict';
 
 
-    angular.module('starter.controllers').controller('AdoptCtrl', ['$scope', '$timeout', '$state', 'treeListService', function($scope, $timeout, $state, treeListService) {
+    angular.module('starter.controllers').controller('AdoptCtrl', ['$scope', '$timeout', '$state', 'treeListService','sharedService', function($scope, $timeout, $state, treeListService,sharedService) {
         $scope.treeList = [];
         $scope.selectedSlideIndex = undefined;
 
@@ -16,6 +16,8 @@
             $scope.currntUsr = JSON.parse(localStorage.getItem('userObj'));
             treeListService.getData().then(function(response) {
                 $scope.treeList = response.data.tree;
+                sharedService.setTreeList($scope.treeList);
+                localStorage.setItem("treeList", JSON.stringify($scope.treeList));
             });
         };
 
--- www/js/controllers/TreeProfileCtrl.js
+++ www/js/controllers/TreeProfileCtrl.js
@@ -1,8 +1,13 @@
-(function(){
+(function() {
     'use strict';
 
-    angular.module('starter.controllers').controller('TreeProfileCtrl', ['$scope', '$timeout', '$state', function($scope, $timeout, $state){
-
+    angular.module('starter.controllers').controller('TreeProfileCtrl', ['$scope', '$timeout', '$state', 'sharedService', function($scope, $timeout, $state, sharedService) {
+        $scope.init = function() {
+            $scope.userDtl = JSON.parse(localStorage.getItem('userObj'));
+            $scope.selectedTree = sharedService.getSelectedTree();
+            console.log($scope.selectedTree);
+        }
+        $scope.init();
         $scope.message = 'Guys do somethong here - from tree ctrl';
 
     }]);
