bf23760675fbca7f86a5efb768dd51e85c400fa2
kaushik <kaushikgotefode@gmail.com>
2016-04-24 00:30:50 +0530

    congrats page and user profile page ui fixes

--- www/js/controllers/AdoptCtrl.js
+++ www/js/controllers/AdoptCtrl.js
@@ -1,51 +1,54 @@
 (function() {
     'use strict';
 
-    angular.module('starter.controllers').controller('AdoptCtrl', ['$scope', '$timeout', '$state', 'treeListService', function($scope, $timeout, $state, treeListService) {
+    angular.module('starter.controllers').controller('AdoptCtrl', ['$scope', '$timeout', '$state', 'treeListService', 'sharedService', function($scope, $timeout, $state, treeListService, sharedService) {
         $scope.treeList = [];
+        $scope.currntUsr = [];
         $scope.selectedSlideIndex = undefined;
-        
+
         /**
-				 * @ngdoc method
-				 * @name init
-				 * @description Initilize default required data
-				 *
-				*/
+         * @ngdoc method
+         * @name init
+         * @description Initilize default required data
+         *
+         */
         $scope.init = function() {
-	        treeListService.getData().then(function(response) {
-	            $scope.treeList = response.data.tree;
-	        });
+
+            $scope.currntUsr = sharedService.getCurrentUser()
+            treeListService.getData().then(function(response) {
+                $scope.treeList = response.data.tree;
+            });
         };
 
         /**
-				 * @ngdoc method
-				 * @name slideHasChanged
-				 * @description Getting index of active slide object
-				 *
-				*/
-        $scope.slideHasChanged = function($index){
-			    console.log('slideHasChanged $index=' + $index);
-			    $scope.selectedSlideIndex = $index+1;
-			  };
+         * @ngdoc method
+         * @name slideHasChanged
+         * @description Getting index of active slide object
+         *
+         */
+        $scope.slideHasChanged = function($index) {
+            console.log('slideHasChanged $index=' + $index);
+            $scope.selectedSlideIndex = $index + 1;
+        };
 
-			  /**
-				 * @ngdoc method
-				 * @name adoptTree
-				 * @description Adopting active tree from slider.
-				 *
-				*/
+        /**
+         * @ngdoc method
+         * @name adoptTree
+         * @description Adopting active tree from slider.
+         *
+         */
         $scope.adoptTree = function() {
-      		if($scope.selectedSlideIndex === undefined) {
-      			$scope.selectedSlideIndex = 1;
-      		}
+            if ($scope.selectedSlideIndex === undefined) {
+                $scope.selectedSlideIndex = 1;
+            }
 
-          for($scope.count=0; $scope.count<$scope.treeList.length; $scope.count++) {
-          	if($scope.treeList[$scope.count].id === $scope.selectedSlideIndex) {
-          		console.log($scope.treeList[$scope.count]);
-          		$state.go("app.congrats", {"adoptedTree": $scope.treeList[$scope.count]});
-          		break;
-          	}
-          }
+            for ($scope.count = 0; $scope.count < $scope.treeList.length; $scope.count++) {
+                if ($scope.treeList[$scope.count].id === $scope.selectedSlideIndex) {
+                    console.log($scope.treeList[$scope.count]);
+                    $state.go("app.congrats", { "adoptedTree": $scope.treeList[$scope.count] });
+                    break;
+                }
+            }
         };
 
         $scope.init();
