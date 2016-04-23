3291add00e89cf81e53dd563f982405c0334ff0c
kaushik <kaushikgotefode@gmail.com>
2016-04-23 23:00:07 +0530

    user profile page

--- www/js/controllers/ProfileCtrl.js
+++ www/js/controllers/ProfileCtrl.js
@@ -1,9 +1,10 @@
-(function(){
-    'use strict';
+// (function() {
+//     'use strict';
 
-    angular.module('starter.controllers').controller('ProfileCtrl', ['$scope', '$timeout', '$state', function($scope, $timeout, $state){
+angular.module('starter.controllers').controller('ProfileCtrl', ['$scope', '$timeout', '$state','sharedService', function($scope, $timeout, $state,sharedService) {
+	$scope.userDtl=sharedService.getCurrentUser();
+    $scope.message = 'Guys do somethong here - from profile ctrl';
 
-        $scope.message = 'Guys do somethong here - from profile ctrl';
 
-    }]);
-})();
+}]);
+// })();
