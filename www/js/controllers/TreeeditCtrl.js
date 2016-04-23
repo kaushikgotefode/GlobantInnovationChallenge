bdb082bbc557f7dacbbb58889db946de4efb3d57
manoj victor <fmanojvictor@gmail.com>
2016-04-23 17:43:07 +0530

    app flow comleted

---
+++ www/js/controllers/TreeeditCtrl.js
@@ -0,0 +1,9 @@
+(function(){
+    'use strict';
+
+    angular.module('starter.controllers').controller('TreeeditCtrl', ['$scope', '$timeout', '$state', function($scope, $timeout, $state){
+
+        $scope.message = 'Guys do somethong here - from tree ctrl';
+
+    }]);
+})();
